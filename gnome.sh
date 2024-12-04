17b58db149e65c714aa7b5ac9e4b6aac87ef801e
cwsacnabuse <135017648+cwsacnabuse@users.noreply.github.com>
2024-12-04 18:03:23 +0530

    Update and rename script.sh to gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -4,27 +4,34 @@ echo "Executing commands ..."
 
 runcommand ()
 {
+
 #Fixing broken ubuntu dependencies for some cases 
   sudo apt update
-  sudo apt upgrade
+  sudo apt upgrade -y
   sudo apt --fix-broken install -y
-#Installing Ubuntu Gnome Desktop
+
+  #Installing Gnome Desktop
   sudo apt install ubuntu-gnome-desktop -y
-  echo "Installing Gnome - Complete"
+  echo "\e[32mInstalling Gnome - Complete\e[0m"
+
 #Installing Chrome Remote Desktop (CRD)
   curl -o /tmp/chrome-remote-desktop_current_amd64.deb https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && \
   sudo apt install -y /tmp/chrome-remote-desktop_current_amd64.deb
-  echo "Installing Chrome Remote Desktop - Complete"
+  echo "\e[32mInstalling Chrome Remote Desktop - Complete\e[0m"
+
 #Installing Chrome Browser (latest)
   wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i google-chrome-stable_current_amd64.deb
-  echo "Installing Chrome - Complete"
+echo "\e[32mInstalling Chrome Browser - Complete\e[0m"
+
 #Installing p7Zip Desktop
   sudo snap install p7zip-desktop
-  echo "Installing 7Zip - Complete"
+  echo "\e[32mInstalling 7Zip - Complete\e[0m"
+  
 #Updating Gnome and reinstalling CRD to fix the broken input issue
   sudo apt autoremove -y
   sudo apt update
   sudo apt-get install chrome-remote-desktop
+  
 }
 
 runcommand
